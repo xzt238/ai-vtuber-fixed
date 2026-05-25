7c17fa80c23483137819ff4bd34b4d0e2a1e564c
泽涛 许 <3022256702@QQ.com>
2026-05-25 21:54:43 +0800

    v1.11.4: PyInstaller打包成功 (65MB exe)

--- native/build.bat
+++ native/build.bat
@@ -5,7 +5,7 @@ REM  Must run with Python 3.11
 REM ======================================
 
 echo ========================================
-echo  Building GuguGagaNative v1.11.1
+echo  Building GuguGagaNative v1.11.4
 echo ========================================
 
 REM Check Python 3.11
